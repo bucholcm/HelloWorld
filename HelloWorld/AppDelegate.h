04d225be044f276252b1cacd763b2404e81e85d8
bucholcm <maciek.bucholc@gmail.com>
2012-11-01 22:18:34 +0100

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Maciej Bucholc on 01.11.2012.
+//  Copyright (c) 2012 Maciej Bucholc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Maciej Bucholc on 01.11.2012.
+//  Copyright (c) 2012 Maciej Bucholc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
